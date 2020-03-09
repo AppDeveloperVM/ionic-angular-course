882f274a6687e042635d9a3d372a0cca46593e10
AppDeveloperVM <vicmr1994@gmail.com>
2020-03-09 18:53:43 +0100

    Added routing paths
    app routing module paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)
-  },
-  {
-    path: 'places',
-    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
-  },
-  {
-    path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
-  }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)},
+  { path: 'places', loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)},
+  { path: 'discover', loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule)},
+  { path: 'offers', loadChildren: () => import('./places/offers/offers.module').then( m => m.OffersPageModule)},
+  { path: 'new-offer', loadChildren: () => import('./places/offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)},
+  { path: 'edit-offer', loadChildren: () => import('./places/offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)},
+  { path: 'place-detail', loadChildren: () => import('./places/discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)},
+  { path: 'offer-bookings', loadChildren: () => import('./places/offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)},
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)},
+
 ];
 
 @NgModule({
