fb63c31b6087f7adb07b88dd61769749ad070da9
AppDeveloperVM <vicmr1994@gmail.com>
2020-03-09 19:35:52 +0100

    update app-routing - NO Simple routing
    Simple routing gives errors - path not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
-  { path: 'auth', loadChildren: './auth/auth.module#.AuthPageModule'},
-  { path: 'places', loadChildren: './places/places.module#.PlacesPageModule'},
-  { path: 'bookings', loadChildren: './bookings/bookings.module#.BookingsPageModule'},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)},
+  { path: 'places', loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)},
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)},
 
 ];
 
