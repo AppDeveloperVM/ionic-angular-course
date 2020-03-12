83f1372efb387c4de2c396a544328586c6e0e53f
AppDeveloperVM <vicmr1994@gmail.com>
2020-03-12 13:03:29 +0100

    Revert "Added 'Navigate BACK' functionality"
    This reverts commit 264beb6f82238ee360e5757831c83658a49cceb3.

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,15 +7,9 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onBookPlace() {
-    // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover');
-    // this.navCtrl.pop();
   }
 
 }
